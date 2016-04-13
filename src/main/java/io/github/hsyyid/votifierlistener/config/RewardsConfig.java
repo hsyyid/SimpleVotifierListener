dc28d5734b3e72c0897b6e0795ffeffe3928b20b
rootatdebian <rootatdebian@gmail.com>
2016-04-13 19:32:15 -0400

    Make the config populate the "mode" option
    If the rewards config file doesn't exist, populate the "mode" option with its default value of "random".

--- src/main/java/io/github/hsyyid/votifierlistener/config/RewardsConfig.java
+++ src/main/java/io/github/hsyyid/votifierlistener/config/RewardsConfig.java
@@ -83,10 +83,11 @@ public class RewardsConfig implements Configurable
 	@Override
 	public void populate()
 	{
-		get().getNode("rewards", "amount").setValue(2).setComment("Number of rewards a player will receive.");
+		get().getNode("rewards", "amount").setValue(2).setComment("Number of random rewards a player will receive.");
 		get().getNode("rewards", "commands").setValue("minecraft:give @p minecraft:diamond_sword 1,").setComment("Possible reward commands for voting.");
 		get().getNode("rewards", "money", "maximum").setValue(500).setComment("Maximum amount of money a player can get for voting.");
 		get().getNode("rewards", "money", "minimum").setValue(100).setComment("Minimum amount of money a player can get for voting.");
+		get().getNode("rewards", "mode").setValue("random").setComment("Mode for giving rewards. Set to 'all' to give one of each, set to 'random' to give 'amount' number of random rewards.");
 	}
 
 	@Override
